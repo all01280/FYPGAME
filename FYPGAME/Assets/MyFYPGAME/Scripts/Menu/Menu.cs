76d249308e0e9ba92876ba1bb9f1ff40e34fc07a
all01280 <71807347+all01280@users.noreply.github.com>
2021-03-25 14:11:16 +0800

    Updata Menu

---
+++ FYPGAME/Assets/MyFYPGAME/Scripts/Menu/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void GoToSetting()
+    {
+        SceneManager.LoadScene("Setting");
+    }
+}
